a167cc6f8b29c0296545ed38eeafd7909438d860
Abhabongse Janthong <6845502+abhabongse@users.noreply.github.com>
2018-11-21 15:47:38 +0700

    Fix formatting of godoc-style docstrings

--- pkg/fuzzymatch/normalization.go
+++ pkg/fuzzymatch/normalization.go
@@ -2,10 +2,10 @@ package fuzzymatch
 
 import "strings"
 
-/**
- * Remove leading and trailing white-spaces, then reduce all inter-word white-spaces
- * into a single normal space.
- */
+/*
+Remove leading and trailing white-spaces, then reduce all inter-word white-spaces
+into a single normal space.
+*/
 func NormalizeWhiteSpaces(str string) string {
 	return strings.Join(strings.Fields(str), " ")
 }
--- pkg/fuzzymatch/scoring.go
+++ pkg/fuzzymatch/scoring.go
@@ -1,10 +1,14 @@
+/*
+Package fuzzymatch implements an approximate string matching algorithm to determine the similarity
+score between two given strings. In most cases, you would be interested in the function SimilarityScore.
+*/
 package fuzzymatch
 
-/**
- * A definitive function which computes the similarity score between two input strings.
- * The returned score value is a floating point between 0 (strings are very distinct)
- * and 1 (strings are identical).
- */
+/*
+A definitive function which computes the similarity score between two input strings.
+The returned score value is a floating point between 0 (strings are very distinct)
+and 1 (strings are identical).
+*/
 func SimilarityScore(fst, snd string) float64 {
 	fstRunes := normalizeString(fst)
 	sndRunes := normalizeString(snd)
@@ -17,9 +21,9 @@ func SimilarityScore(fst, snd string) float64 {
 	return 0
 }
 
-/**
- * Normalize an input string using various methods and return as a slice of runes.
- */
+/*
+Normalize an input string using various methods and return as a slice of runes.
+*/
 func normalizeString(str string) []rune {
 	// TODO: introduce multiple string normalization functions
 	str = NormalizeWhiteSpaces(str)
