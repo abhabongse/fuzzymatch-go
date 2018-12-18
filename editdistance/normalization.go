1efea044a3adae659aa391a07be3e8255d9696bd
Abhabongse Janthong <6845502+abhabongse@users.noreply.github.com>
2018-12-18 16:49:14 +0700

    Clarify the normalization of the editdistance functions

--- editdistance/normalization.go
+++ editdistance/normalization.go
@@ -5,17 +5,22 @@ import (
 )
 
 /*
-MakeNormalized converts a typical edit-distance computation function in the
-string space into a normalized version. Thus, the returned score of the
-new edit-distance function should always be between 0 (meaning two strings
-are very similar) and 1 (meaning two strings are very different).
+MakeNormalized converts a typical edit-distance computation function in the string space
+into its normalized version. Therefore, the returned score of the new edit-distance
+function should always be between 0 (meaning two strings are very similar) and 1
+(meaning two strings are very different).
+
+Implementation details: the denominator of the fraction is not the length of the longer
+string. The reason for this is that some insertion/deletion errors incur sub-unit penalties.
+Without the size-fitting denominator, a malicious user may attack by saturating those
+insertions/deletions in order to decrease the total edit distances.
 */
 func MakeNormalized(computeDistance StringDistanceFunction) StringDistanceFunction {
 	computeNormalizedDistance := func(fst, snd string) float64 {
 		dist := computeDistance(fst, snd)
-		fstLength := computeDistance(fst, "")
-		sndLength := computeDistance("", snd)
-		score := dist / math.Max(fstLength, sndLength)
+		fstCapacity := computeDistance(fst, "")
+		sndCapacity := computeDistance("", snd)
+		score := dist / math.Max(fstCapacity, sndCapacity)
 		if math.IsNaN(score) {
 			score = 0.0 // both strings are empty
 		}
