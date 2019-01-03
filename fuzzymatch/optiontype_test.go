d080b276d4ba7d352d8763dcb26f8c3148f521aa
Abhabongse Janthong <6845502+abhabongse@users.noreply.github.com>
2019-01-03 12:49:53 +0700

    Add test for panic for functional options in NewSimilarityScoreFunction

--- fuzzymatch/optiontype_test.go
+++ fuzzymatch/optiontype_test.go
@@ -2,6 +2,8 @@ package fuzzymatch
 
 import (
 	"fmt"
+	"testing"
+
 	"github.com/abhabongse/fuzzymatch-go/editdistance"
 )
 
@@ -19,4 +21,38 @@ func ExampleSimilarityScore() {
 	// Constructed string similarity score function can be applied to pairs of strings
 	score := SimilarityScore("saturday", "sunday")
 	fmt.Println(score)
+}
+
+func TestCombinationWeights(t *testing.T) {
+	type args struct {
+		optimalAlignmentWeight float64
+		diceSimilarityWeight   float64
+	}
+	tests := []struct {
+		name        string
+		args        args
+		shouldPanic bool
+	}{
+		{"default", args{1.0, 0.0}, false},
+		{"positive", args{2.0, 3.0}, false},
+		{"all zeros", args{0.0, 0.0}, true},
+		{"negative", args{3.0, -2.0}, true},
+		// TODO: Add test cases.
+	}
+
+	checkPanic := func(args args, shouldPanic bool) {
+		defer func(){
+			if r := recover(); (r != nil) != shouldPanic {
+				t.Errorf("CombinationWeights() panic status = %v, expected %v", r != nil, shouldPanic)
+			}
+		}()
+		NewSimilarityScoreFunction(CombinationWeights(args.optimalAlignmentWeight, args.diceSimilarityWeight))
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Try to run the function and recover from panic
+			checkPanic(tt.args, tt.shouldPanic)
+		})
+	}
 }
